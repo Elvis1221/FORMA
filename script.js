a007732d0fb5280eeda3d7239f0c28e0a99ddadb
Andrei <fujitsuelvis@gmail.com>
2020-11-04 18:29:42 +0300

    homework - FormRegistration

---
+++ script.js
@@ -0,0 +1,16 @@
+const addLastName = document.form.lastName;
+const addFirstName = document.form.firstName;
+const addDate = document.form.date;
+const addEmail = document.form.email;
+
+const addButton = document.form.button;
+
+addButton.addEventListener('click', function () {
+    const addForm = {
+        LastName: addLastName.value,
+        FirstName: addFirstName.value,
+        Date: addDate.value,
+        Email: addEmail.value,
+    };
+    console.log(addForm);
+});
